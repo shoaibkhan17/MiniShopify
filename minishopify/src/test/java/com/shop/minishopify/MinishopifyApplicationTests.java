beef5f365031d47d0e1f60b12bf1381f737557fb
Meet Digrajkar <meetdigrajkar@cmail.com>
2021-03-05 15:39:43 -0500

    initial maven project setup

---
+++ minishopify/src/test/java/com/shop/minishopify/MinishopifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.minishopify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinishopifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
