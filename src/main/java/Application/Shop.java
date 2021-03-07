4ca751ebfcd6318f29b04224276298fe47205338
Abdillahi Nur <abdillahimnur@cmail.carleton.ca>
2021-03-06 19:18:24 -0500

    Set up framework for creating shop and editing shop html pages

--- src/main/java/Application/Shop.java
+++ src/main/java/Application/Shop.java
@@ -1,2 +1,50 @@
-package Application;public class Shop {
+package Application;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Shop {
+    private String name;
+    private String description;
+    private int storeId;
+
+
+    public Shop(String name, String description, int storeId) {
+        this.name = name;
+        this.description = description;
+        this.storeId = storeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "name ='" + name + '\'' +
+                ", description ='" + description + '\'' +
+                ", storeId =" + storeId +
+                '}';
+    }
 }
