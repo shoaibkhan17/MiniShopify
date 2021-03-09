026ba7db3f526b7789e3f0c1b5b9b359031eb4c4
Abdillahi Nur <abdillahimnur@cmail.carleton.ca>
2021-03-08 19:51:03 -0500

    Updating files

--- src/main/java/Application/Shop.java
+++ src/main/java/Application/Shop.java
@@ -1,18 +1,37 @@
 package Application;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+
 
 @Entity
 public class Shop {
     private String name;
     private String description;
+    private ArrayList<Item> shopItems;
     private int storeId;
-
 
     public Shop(String name, String description, int storeId) {
         this.name = name;
         this.description = description;
         this.storeId = storeId;
+        shopItems = new ArrayList<Item>();
+    }
+
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
+    public ArrayList<Item> getShopItems() {
+        return shopItems;
+    }
+
+    public void setShopItems(ArrayList<Item> shopItems) {
+        this.shopItems = shopItems;
+    }
+
+    public void addShopItems(Item item){
+        shopItems.add(item);
     }
 
     public String getName() {
