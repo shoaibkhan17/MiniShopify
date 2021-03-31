8834c0ca9e3983c37ff153a6831b8ed81d75ddc3
Meet Digrajkar <meetdigrajkar@cmail.carleton.ca>
2021-03-31 19:01:14 -0400

    Added check out endpoint
    -checkout end point (takes map of product id and quantity)
    -returns new map of product ids and quantities
    -

--- src/main/java/controllers/CartController.java
+++ src/main/java/controllers/CartController.java
@@ -1,21 +1,15 @@
 package controllers;
 
-import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import config.FirebaseService;
-import models.Product;
-import models.Shop;
 
 @CrossOrigin
 @RestController
@@ -25,18 +19,8 @@ public class CartController {
 	@Autowired
 	FirebaseService firebaseService;
 
-	@GetMapping("/getProducts")
-    public List<Product> getProducts() throws ExecutionException, InterruptedException {
-		return null;
-    }
-	
-	@PostMapping("/addProduct")
-    public ResponseEntity<String> removeProduct(@RequestBody Shop shop) throws ExecutionException, InterruptedException {
-		return new ResponseEntity<String>("Product added: ", HttpStatus.OK);
-    }
-	
-	@PostMapping("/removeProduct")
-    public ResponseEntity<String> updateShop(@RequestBody Shop shop) throws ExecutionException, InterruptedException {
-		return new ResponseEntity<String>("Product removed: ", HttpStatus.OK);
+	@GetMapping("/checkOut")
+    public Map<String, Integer> checkOutProducts(@RequestBody Map<String,Integer> cartProducts) throws ExecutionException, InterruptedException {
+		return firebaseService.checkOutProducts(cartProducts);
     }
 }
