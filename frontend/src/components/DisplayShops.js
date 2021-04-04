e01bf01d92f17f29fc6b5f221406dd0ab8ee152c
Shoaib Khan <shoaib.arifkhan@gmail.com>
2021-04-03 22:57:36 -0400

    Revert "Added Search and Filter functionality"
    This reverts commit 16baaa3b30d7aea6c402b6ace9f3cc8b36ad2074.

--- frontend/src/components/DisplayShops.js
+++ frontend/src/components/DisplayShops.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Grid, TextField, IconButton } from "@material-ui/core";
+import { Grid, IconButton } from "@material-ui/core";
 import { setShops } from "../redux/actions";
 import ShopService from "../services/ShopService";
 import Shop from "./Shop";
 import AddCircleRoundedIcon from "@material-ui/icons/AddCircleRounded";
 import AddShop from "./AddShop";
-import Select from "@material-ui/core/Select";
-import NativeSelect from "@material-ui/core/NativeSelect";
 
 const mapStateToProps = (state) => {
   return { shops: state.shops };
@@ -17,28 +15,15 @@ class DisplayShops extends React.Component {
     super(props);
     this.state = {
       addingShop: false,
-      shops: [],
-      tags: [],
-      selectedTag: "",
-      searchValue: "",
     };
 
     this.getShops = this.getShops.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSelectChange = this.handleSelectChange.bind(this);
     this.addNewShop = this.addNewShop.bind(this);
     this.closeAddShop = this.closeAddShop.bind(this);
   }
 
   componentDidMount() {
-    var tagsList = new Set();
-    this.props.shops.forEach((shop) => {
-      shop.tags.forEach((tag) => {
-        tagsList.add(tag);
-      });
-    });
-    console.log(...tagsList);
-    this.setState({ shops: this.props.shops, tags: Array.from(tagsList) });
+    this.getShops();
   }
 
   addNewShop() {
@@ -53,56 +38,10 @@ class DisplayShops extends React.Component {
     await ShopService.getAllShops();
   }
 
-  handleChange(event) {
-    this.setState({ selectedTag: "", searchValue: event.target.value });
-    if (event.target.value === "") {
-      this.setState({ shops: this.props.shops });
-    } else {
-      const filteredData = this.props.shops.filter(
-        (shop) => shop.name === event.target.value
-      );
-      this.setState({ shops: filteredData });
-    }
-  }
-
-  handleSelectChange(event) {
-    this.setState({ selectedTag: event.target.value, searchValue: "" });
-    if (event.target.value === "") {
-      this.setState({ shops: this.props.shops });
-    } else {
-      const filteredData = this.props.shops.filter((shop) =>
-        shop.tags.includes(event.target.value)
-      );
-      this.setState({ shops: filteredData });
-    }
-  }
-
   render() {
-    console.log("update");
     return (
       <div>
-        <TextField
-          value={this.state.searchValue}
-          onChange={(event) => {
-            this.handleChange(event);
-          }}
-        />
-        <Select
-          value={this.state.selectedTag}
-          native
-          onChange={(event) => {
-            this.handleSelectChange(event);
-          }}
-        >
-          <option aria-label="None" value="" />
-          {this.state.tags &&
-            this.state.tags.map((tag) => (
-              <option key={tag} value={tag}>
-                {" "}
-                {tag}
-              </option>
-            ))}
-        </Select>
+        {console.log(this.props.shops)}
         <Grid
           container
           justify="center"
@@ -112,8 +51,8 @@ class DisplayShops extends React.Component {
             width: "96%",
           }}
         >
-          {this.state.shops &&
-            this.state.shops.map((shop) => (
+          {this.props.shops &&
+            this.props.shops.map((shop) => (
               <Grid item key={shop.shopID}>
                 <Shop canEditShop={true} canOpen={true} shop={shop} />
               </Grid>
